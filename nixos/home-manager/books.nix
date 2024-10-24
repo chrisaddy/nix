400fbc87d16306b74476a8fc1a0a120e34f77e46
chrisaddy <chris.william.addy@gmail.com>
2024-10-24 17:47:22 -0400

    add g

---
+++ nixos/home-manager/books.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  programs = {
+    sioyek = {
+      enable = true;
+      bindings = {
+  "move_up" = "e";
+  "move_down" = "n";
+  "move_left" = "m";
+  "move_right" = "i";
+  # "screen_down" = [ "<C-n>" ];
+  # "screen_up" = [ "<C-e>" ];
+}
+    };
+  };
+}
